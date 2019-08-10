239cba873b6728b7a88915aacee0dc27002cee12
Sdreamery <sean.xs@foxmail.com>
2019-08-10 22:56:51 +0800

    Site updated: 2019-08-10 22:56:50

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "奇迹的山",
+        artist: '岸部眞明',
+        url: 'https://music.163.com/song?id=441552&userid=123065831',
+      }
+    ]
+});
+
